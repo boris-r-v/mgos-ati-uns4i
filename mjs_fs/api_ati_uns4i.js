ae4ac8f3a73be6e9a0a22171154ecb217188001f
Boris R <borisrozhkin@gmail.com>
2021-05-21 10:58:53 +0500

    Working on js-code

--- mjs_fs/api_ati_uns4i.js
+++ mjs_fs/api_ati_uns4i.js
@@ -1,3 +1,10 @@
-let USN4i_TRC = {
-    _uns4i_answer_descr: ffi("void *get_uns4i_trc_descr()")
+let UNS4i_TRC = {
+    _uns4i_answer_descr: ffi("void *get_uns4i_trc_descr()"),
+    _get_uns4i_data: ffi( "void* get_uns4i_data( char const*  _answer, size_t _len )" ),
+    
+    is_crc_good: ffi( "bool is_crc_good( char const* _answer, size_t _len )" ),
+    
+    get_uns4i_data: function( str, len ) {
+        return s2o( UNS4i_TRC._get_uns4i_data( str, len ),  UNS4i_TRC._uns4i_answer_descr );
+    }
 }
